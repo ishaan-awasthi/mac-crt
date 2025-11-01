d1d6a3cef17f7cd66cdcc622fdd8da95f947ccb9
Ishaan Awasthi <ishaanawasthi05@gmail.com>
2025-10-31 21:11:22 -0700

    Hidden from dock (LSUI==0)

--- mac-crt/mac-crt/mac_crtApp.swift
+++ mac-crt/mac-crt/mac_crtApp.swift
@@ -15,6 +15,17 @@ struct mac_crtApp: App {
             systemImage: "water.waves"
         ) {
             ContentView()
+//                .overlay(alignment: .topTrailing) {
+//                    Button(
+//                        "Quit",
+//                        systemImage: "xmark.circle.fill"
+//                    ) {
+//                        NSApp.terminate(nil)
+//                    }
+//                    .labelStyle(.iconOnly)
+//                    .buttonStyle(.plain)
+//                    .padding(-2)
+//                }
                 .frame(width: 150, height: 75)
         }.menuBarExtraStyle(.window)
     }
